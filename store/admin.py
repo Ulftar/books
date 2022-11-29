f0917ba7ec152edb0e3c78ada46526df4df9f34e
Ulftar <alexandr.kolodnitskiy@gmail.com>
2022-11-29 11:43:54 +0700

    Add book model register at admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from store.models import Book
+
+
+# Регистрация модели книги в админке
+@admin.register(Book)
+class BookAdmin(ModelAdmin):
+    pass
