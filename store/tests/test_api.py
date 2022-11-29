9f0c9f7accc5b29ce27758f6999f11f41b920626
Ulftar <alexandr.kolodnitskiy@gmail.com>
2022-11-29 16:02:50 +0700

    Refactor api test case

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -7,15 +7,21 @@ from store.serializers import BooksSerializer
 
 
 class BooksApiTestCase(APITestCase):
-    def test_get(self):
+    def setUp(self):
         # Создаем с помощью ОРМ тестовый список книг
-        book_1 = Book.objects.create(name='Test book 1', price=25)
-        book_2 = Book.objects.create(name='Test book 2', price=55)
+        self.book_1 = Book.objects.create(name='Test book 1', price=25,
+                                          author_name='Author 1')
+        self.book_2 = Book.objects.create(name='Test book 2', price=55,
+                                          author_name='Author 1')
+        self.book_3 = Book.objects.create(name='Test book 3', price=55,
+                                          author_name='Author 2')
+
+    def test_get(self):
         # Тестируем ответ от url localhost/book/
         url = reverse('book-list')
         response = self.client.get(url)
         # Передаем список объектов для сравнения, аргумент many для сериализации каждого элемента списка
-        serializer_data = BooksSerializer([book_1, book_2], many=True).data
+        serializer_data = BooksSerializer([self.book_1, self.book_2, self.book_3], many=True).data
         self.assertEqual(serializer_data, response.data)
         # Тестируем код ответа сервера
         self.assertEqual(status.HTTP_200_OK, response.status_code)
