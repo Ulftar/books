036de06ac0bcfbf37a1fa7efe33a929359e61413
Ulftar <alexandr.kolodnitskiy@gmail.com>
2022-11-30 16:00:51 +0700

    Add GitHub auth url fix

--- books/urls.py
+++ books/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.template.defaulttags import url
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import SimpleRouter
 
 from store.views import BookViewSet, auth
@@ -26,7 +26,7 @@ router.register(r'book', BookViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('', include('social_django.urls', namespace='social')),
+    re_path('', include('social_django.urls', namespace='social')),
     path('auth/', auth),
 ]
 
