cceebbd7d46d2a79d137f0d95817500eefa13c0d
Ulftar <alexandr.kolodnitskiy@gmail.com>
2022-12-08 16:11:27 +0700

    Add book likes and rate model at admin

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 
 
 # Регистрация модели книги в админке
 @admin.register(Book)
 class BookAdmin(ModelAdmin):
+    pass
+
+
+# Регистрация модели рейтов в админке
+@admin.register(UserBookRelation)
+class UserBookRelationAdmin(ModelAdmin):
     pass
