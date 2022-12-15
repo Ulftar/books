136f6366c68c707973ff9384dd36d14fb93228ef
Ulftar <alexandr.kolodnitskiy@gmail.com>
2022-12-15 10:48:06 +0700

    Add relation serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 
 
+# API для книг
 class BooksSerializer(ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+
+# API для системы рейтов
+class UserBookRelationSerializer(ModelSerializer):
+    class Meta:
+        model = UserBookRelation
+        fields = ('book', 'like', 'in_bookmarks', 'rate')
