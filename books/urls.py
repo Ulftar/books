bc89e73bb14a71725a8d92036356a185e148cfc9
Ulftar <alexandr.kolodnitskiy@gmail.com>
2023-01-11 10:14:44 +0700

    Add router for book relation

--- books/urls.py
+++ books/urls.py
@@ -18,11 +18,13 @@ from django.template.defaulttags import url
 from django.urls import path, include, re_path
 from rest_framework.routers import SimpleRouter
 
-from store.views import BookViewSet, auth
+from store.views import BookViewSet, auth, UserBooksRelationView
 
 router = SimpleRouter()
 
 router.register(r'book', BookViewSet)
+router.register(r'book_relation', UserBooksRelationView)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
